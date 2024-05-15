fd00e995f2bdd4065e070a3114490a3b53cd08fe
acl13 <128934431+acl13@users.noreply.github.com>
2024-05-15 08:08:53 -0400

    complete findSum

---
+++ submission.js
@@ -0,0 +1,30 @@
+const findSum = function(array) {
+    let sum = 0;
+    for (num of array) {
+        sum += num;
+    }
+    return sum;
+  };
+  
+
+  const findFrequency = function(array) {
+    // your code here - don't forget to return an object!
+  };
+  
+  const isPalindrome = function(str) {
+    // your code here - don't forget to return a boolean!
+ 
+  };
+
+  
+  const largestPair = function(array) {
+    // your code here - don't forget to return a number!
+  };
+  
+  const removeParenth = function(str) {
+    // your code here - don't forget to return a string!
+  };
+  
+  const scoreScrabble = function(str) {
+    // your code here - don't forget to return a number!
+  };
